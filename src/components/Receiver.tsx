5c00df18da199d139b3e6d2381431c61ddc45bd5
kuchbhimingwal <shubhammingi@gmail.com>
2024-07-05 13:06:21 -0400

    connected both the browsers

--- src/components/Receiver.tsx
+++ src/components/Receiver.tsx
@@ -1,6 +1,51 @@
 import { useEffect, useState } from "react"
+import { useRef } from 'react'
+type Draw = {
+  ctx: CanvasRenderingContext2D
+  currentPoint: Point
+  prevPoint: Point | null
+}
+
+type Point = { x: number; y: number } | null
 
 function Receiver() {
+  const [currentPoint, setCurrentPoint] = useState(null)
+  const [prevPoint, setPrevPoint] = useState(null)
+  const [color, setColor] = useState("#000000")
+  const canvasRef = useRef(null);
+  // @ts-ignore
+  const ctx = canvasRef.current?.getContext('2d');
+  
+  drawLine({ ctx, currentPoint, prevPoint })
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    if(!prevPoint || !currentPoint) return
+    const { x: currX, y: currY } = currentPoint
+    // setPrevPoint(prevPoint);
+    // setCurrentPoint(currentPoint);
+    console.log(prevPoint);
+    console.log(currentPoint);
+    
+    
+    const lineColor = color
+    const lineWidth = 5
+  
+    let startPoint = prevPoint ?? currentPoint
+    ctx.beginPath()
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = lineColor
+    ctx.moveTo(startPoint.x, startPoint.y)
+  
+    ctx.lineTo(currX, currY)
+    ctx.stroke()
+  
+    ctx.fillStyle = lineColor
+    ctx.beginPath()
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+  
+  
+
   const [socket, setSocket] = useState<WebSocket | null>(null);
     useEffect(()=>{
       const socket = new WebSocket('ws://localhost:8080');
@@ -8,9 +53,28 @@ function Receiver() {
       socket.onopen = () =>{
         socket.send(JSON.stringify({type: "receiver"}));
       }
+      
+      socket.onmessage = async(event) => {
+        // @ts-ignore
+        const message = JSON.parse(event.data)
+        console.log(message);
+        setCurrentPoint(message.sdp.currentPoint);
+        setPrevPoint(message.sdp.prevPoint);
+        setColor(message.sdp.color)
+        return () => socket.close();
+    }
     },[]);
+
+    
   return (
-    <div>Receiver</div>
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={750}
+        height={750}
+        className='border border-black rounded-md'
+      />
+    </div>
   )
 }
 
--- src/components/Sender.tsx
+++ src/components/Sender.tsx
@@ -1,8 +1,51 @@
 import { useEffect, useState } from "react"
+import { useDraw } from '../../hooks/useDraw'
+import { ChromePicker } from 'react-color'
+type Draw = {
+  ctx: CanvasRenderingContext2D
+  currentPoint: Point
+  prevPoint: Point | null
+}
+
+type Point = { x: number; y: number }
+interface pageProps {}
 
 
 function Sender() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  const [color, setColor] = useState<string>('#000')
+  const { canvasRef, onMouseDown, clear } = useDraw(drawLine)
+  const [prevPoint, setPrevPoint] = useState<Point | null>(null)
+  const [currentPoint, setCurrentPoint] = useState<Point | null>(null)
+
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint
+    setPrevPoint(prevPoint);
+    setCurrentPoint(currentPoint);
+    if(prevPoint && currentPoint){
+      socket?.send(JSON.stringify({ type: 'createLine', sdp: {prevPoint, currentPoint, color} }));
+    }
+    
+    const lineColor = color
+    const lineWidth = 5
+
+    let startPoint = prevPoint ?? currentPoint
+    ctx.beginPath()
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = lineColor
+    ctx.moveTo(startPoint.x, startPoint.y)
+
+    ctx.lineTo(currX, currY)
+    ctx.stroke()
+
+    ctx.fillStyle = lineColor
+    ctx.beginPath()
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+
     useEffect(()=>{
       const socket = new WebSocket('ws://localhost:8080');
       setSocket(socket);
@@ -11,7 +54,21 @@ function Sender() {
       }
     },[]);
   return (
-    <div>Sender</div>
+    <div className='bg-white flex'>
+      <div className='pr-10'>
+        <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+        <button type='button' className='p-2 rounded-md border border-black' onClick={clear}>
+          Clear canvas
+        </button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        width={750}
+        height={750}
+        className='border border-black rounded-md'
+      />
+    </div>
   )
 }
 
